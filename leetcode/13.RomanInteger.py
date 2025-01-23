95508906095ddc0ca3254405260f164c987e7442
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-01-23 14:04:46 +0100

    working with simple cases

---
+++ leetcode/13.RomanInteger.py
@@ -0,0 +1,65 @@
+"""Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+
+Example 1:
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+
+Example 2:
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Example 3:
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+"""
+
+
+def romanToInt(s: str) -> int:
+    roman_to_int = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    number = 0
+    for letter in s:
+        
+        match letter:
+            case "I":
+                number += 1
+
+            case "V":
+                number += 5
+
+            case "X":
+                number += 10
+
+            case "L":
+                number += 50
+
+            case "C":
+                number += 100
+
+            case "D":
+                number += 500
+
+            case "M":
+                number += 1000
+    return number
+
+
+result1 = romanToInt("III")
+assert result1 == 3, f"Should be 3, it is {result1}"
+
+result2 = romanToInt("LVIII")
+assert result2 == 58, f"Should be 58, it is {result2}"
+
+result3 = romanToInt("MCMXCIV")
+assert result3 == 1994, f"Should be 1994, it is {result3}"
+
+print("Well done.")
