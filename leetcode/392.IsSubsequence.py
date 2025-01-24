54df4633ef3d1eeb2bbb4271f679f413814ab8fc
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-01-24 13:02:19 +0100

    done

---
+++ leetcode/392.IsSubsequence.py
@@ -0,0 +1,45 @@
+"""
+https://leetcode.com/problems/is-subsequence/description/
+
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+A subsequence of a string is a new string that is formed from the original string by
+deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. 
+(i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+"""
+
+
+def isSubsequence(s: str, t: str) -> bool:
+    if s: # because if 's' is an empty string then the algorithm would try to get s[0] which doesnt exist 
+        letter_to_find = 0
+        subsequence = ""
+        for char in t:
+            if char == s[letter_to_find]:
+                subsequence += s[letter_to_find]
+                if len(subsequence) == len(s):
+                    return True
+                else:
+                    letter_to_find += 1
+        return False
+    return True
+
+
+example1 = isSubsequence(s="abc", t="ahbgdc")
+assert example1 == True, "Should be true."
+
+example2 = isSubsequence(s="axc", t="ahbgdc")
+assert example2 == False, "Should be false."
+
+example3 = isSubsequence(s="", t="ahbgdc")
+assert example3 == False, "Should be true."
